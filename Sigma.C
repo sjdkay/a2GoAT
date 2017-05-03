5cacafc476f71c56a444b1b184b71eb6a076715a
sjdkay <sjdkay@gmail.com>
2017-05-03 23:44:27 +0100

    Fixed sigma macro and added rest of plots, commented out multi plotter for now

--- Sigma.C
+++ Sigma.C
@@ -5,7 +5,7 @@ void Sigma(){
   TFile *f1= TFile::Open("/scratch/Mainz_Software/a2GoAT/ParaPerpAsymm_Total_10.root");
   TTree *t1 = (TTree*)f1->Get("Parameter_Values");
 
-  Double_t nValues410[10], nValues430[10], nValues450[10], nValues470[10], nValues490[10], nValues510[10], nValues530[10], nValues550[10], nValues570[10], nValues590[10], nValues610[10], nValues630[10];
+  Double_t pValues410[10], pValues430[10], pValues450[10], pValues470[10], pValues490[10], pValues510[10], pValues530[10], pValues550[10], pValues570[10], pValues590[10], pValues610[10], pValues630[10];
   Double_t nValues410[10], nValues430[10], nValues450[10], nValues470[10], nValues490[10], nValues510[10], nValues530[10], nValues550[10], nValues570[10], nValues590[10], nValues610[10], nValues630[10];
   Double_t nErrValues410[10], nErrValues430[10], nErrValues450[10], nErrValues470[10], nErrValues490[10], nErrValues510[10], nErrValues530[10], nErrValues550[10], nErrValues570[10], nErrValues590[10], nErrValues610[10], nErrValues630[10];
   Double_t nErrValues410[10], nErrValues430[10], nErrValues450[10], nErrValues470[10], nErrValues490[10], nErrValues510[10], nErrValues530[10], nErrValues550[10], nErrValues570[10], nErrValues590[10], nErrValues610[10], nErrValues630[10];
@@ -210,11 +210,11 @@ void Sigma(){
   hr = canvas->DrawFrame(xMin,yMin,xMax,yMax);
   hr->SetTitle("#Sigma as fn of Cos#theta_{CM} (E_{#gamma} 400-420MeV)");
 
-  gr1 = new TGraphErrors(12, x, pSigmaValues410, ex, pSigmaErrValues410);
+  gr1 = new TGraphErrors(10, x, pSigmaValues410, ex, pSigmaErrValues410);
   gr1->SetMarkerColor(2);
   gr1->SetMarkerStyle(21);
   gr1->SetMarkerSize(2);
-  gr2 = new TGraphErrors(12, x, nSigmaValues410, ex, nSigmaErrValues410);
+  gr2 = new TGraphErrors(10, x, nSigmaValues410, ex, nSigmaErrValues410);
   gr2->SetMarkerColor(4);
   gr2->SetMarkerStyle(22);
   gr2->SetMarkerSize(2);
@@ -243,11 +243,11 @@ void Sigma(){
   hr1 = canvas1->DrawFrame(xMin,yMin/2,xMax,yMax/2);
   hr1->SetTitle("#Sigma as fn of Cos#theta_{CM} (E_{#gamma} 420-440MeV)");
 
-  gr3 = new TGraphErrors(12, x, pSigmaValues430 , ex, pSigmaErrValues430);
+  gr3 = new TGraphErrors(10, x, pSigmaValues430 , ex, pSigmaErrValues430);
   gr3->SetMarkerColor(2);
   gr3->SetMarkerStyle(21);
   gr3->SetMarkerSize(2);
-  gr4 = new TGraphErrors(12, x, nSigmaValues430, ex, nSigmaErrValues430);
+  gr4 = new TGraphErrors(10, x, nSigmaValues430, ex, nSigmaErrValues430);
   gr4->SetMarkerColor(4);
   gr4->SetMarkerStyle(22);
   gr4->SetMarkerSize(2);
@@ -276,11 +276,11 @@ void Sigma(){
   hr2 = canvas2->DrawFrame(xMin,yMin/2,xMax,yMax/2);
   hr2->SetTitle("#Sigma as fn of Cos#theta_{CM} (E_{#gamma} 440-460MeV)");
 
-  gr5 = new TGraphErrors(12, x, pSigmaValues450 , ex, pSigmaErrValues450);
+  gr5 = new TGraphErrors(10, x, pSigmaValues450 , ex, pSigmaErrValues450);
   gr5->SetMarkerColor(2);
   gr5->SetMarkerStyle(21);
   gr5->SetMarkerSize(2);
-  gr6 = new TGraphErrors(12, x, nSigmaValues450, ex, nSigmaErrValues450);
+  gr6 = new TGraphErrors(10, x, nSigmaValues450, ex, nSigmaErrValues450);
   gr6->SetMarkerColor(4);
   gr6->SetMarkerStyle(22);
   gr6->SetMarkerSize(2);
@@ -309,11 +309,11 @@ void Sigma(){
   hr3 = canvas3->DrawFrame(xMin,yMin/2,xMax,yMax/2);
   hr3->SetTitle("#Sigma as fn of Cos#theta_{CM} (E_{#gamma} 460-480MeV)");
 
-  gr7 = new TGraphErrors(12, x, pSigmaValues470 , ex, pSigmaErrValues470);
+  gr7 = new TGraphErrors(10, x, pSigmaValues470 , ex, pSigmaErrValues470);
   gr7->SetMarkerColor(2);
   gr7->SetMarkerStyle(21);
   gr7->SetMarkerSize(2);
-  gr8 = new TGraphErrors(12, x, nSigmaValues470, ex, nSigmaErrValues470);
+  gr8 = new TGraphErrors(10, x, nSigmaValues470, ex, nSigmaErrValues470);
   gr8->SetMarkerColor(4);
   gr8->SetMarkerStyle(22);
   gr8->SetMarkerSize(2);
@@ -342,11 +342,11 @@ void Sigma(){
   hr4 = canvas4->DrawFrame(xMin,yMin/2,xMax,yMax/2);
   hr4->SetTitle("#Sigma as fn of Cos#theta_{CM} (E_{#gamma} 480-500MeV)");
 
-  gr9 = new TGraphErrors(12, x, pSigmaValues490, ex, pSigmaErrValues490);
+  gr9 = new TGraphErrors(10, x, pSigmaValues490, ex, pSigmaErrValues490);
   gr9->SetMarkerColor(2);
   gr9->SetMarkerStyle(21);
   gr9->SetMarkerSize(2);
-  gr10 = new TGraphErrors(12, x, nSigmaValues490, ex, nSigmaErrValues490);
+  gr10 = new TGraphErrors(10, x, nSigmaValues490, ex, nSigmaErrValues490);
   gr10->SetMarkerColor(4);
   gr10->SetMarkerStyle(22);
   gr10->SetMarkerSize(2);
@@ -375,11 +375,11 @@ void Sigma(){
   hr5 = canvas5->DrawFrame(xMin,yMin,xMax,yMax);
   hr5->SetTitle("#Sigma as fn of Cos#theta_{CM} (E_{#gamma} 500-520MeV)");
 
-  gr11 = new TGraphErrors(12, x, pSigmaValues510, ex, pSigmaErrValues510);
+  gr11 = new TGraphErrors(10, x, pSigmaValues510, ex, pSigmaErrValues510);
   gr11->SetMarkerColor(2);
   gr11->SetMarkerStyle(21);
   gr11->SetMarkerSize(2);
-  gr12 = new TGraphErrors(12, x, nSigmaValues510, ex, nSigmaErrValues510);
+  gr12 = new TGraphErrors(10, x, nSigmaValues510, ex, nSigmaErrValues510);
   gr12->SetMarkerColor(4);
   gr12->SetMarkerStyle(22);
   gr12->SetMarkerSize(2);
@@ -396,19 +396,229 @@ void Sigma(){
   leg6->Draw("Same");
 
   TCanvas *canvas6 = new TCanvas("canvas6","canvas6", 1920, 1080);
-  canvas6->Divide(3,2);
-  canvas6->cd(1);
-  pad1->Draw();
-  canvas6->cd(2);
-  pad2->Draw();
-  canvas6->cd(3);
-  pad3->Draw();
-  canvas6->cd(4);
-  pad4->Draw();
-  canvas6->cd(5);
-  pad5->Draw();
-  canvas6->cd(6);
-  pad6->Draw();
+  TPad *pad7 = new TPad("pad7","",0,0,1,1);
+  pad7->Draw();
+  pad7->cd();
+
+  pad7->SetTickx(1);
+  pad7->SetTicky(1);
+  pad7->SetGridx(1);
+  pad7->SetGridy(1);
+  TH1F  *hr6;
+  hr6 = canvas6->DrawFrame(xMin,yMin,xMax,yMax);
+  hr6->SetTitle("#Sigma as fn of Cos#theta_{CM} (E_{#gamma} 520-540MeV)");
+
+  gr13 = new TGraphErrors(10, x, pSigmaValues510, ex, pSigmaErrValues510);
+  gr13->SetMarkerColor(2);
+  gr13->SetMarkerStyle(21);
+  gr13->SetMarkerSize(2);
+  gr14 = new TGraphErrors(10, x, nSigmaValues510, ex, nSigmaErrValues510);
+  gr14->SetMarkerColor(4);
+  gr14->SetMarkerStyle(22);
+  gr14->SetMarkerSize(2);
+  TMultiGraph *mg7 = new TMultiGraph();
+  mg7->Add(gr13, "ep");
+  mg7->Add(gr14, "ep");
+  mg7->SetTitle("#Sigma as fn of Cos#theta_{CM} (E_{#gamma} 520-540MeV)");
+  mg7->GetXaxis()->SetTitle("Cos#theta_{CM}");
+  mg7->GetYaxis()->SetTitle("#Sigma");
+  leg7 = new TLegend(0.75, 0.75, 0.95, 0.95); // Add legend to plot
+  leg7->AddEntry(gr13, "Proton", "ep");
+  leg7->AddEntry(gr14, "Neutron", "ep");
+  mg7->Draw();
+  leg7->Draw("Same");
+
+  TCanvas *canvas7 = new TCanvas("canvas7","canvas7", 1920, 1080);
+  TPad *pad8 = new TPad("pad8","",0,0,1,1);
+  pad8->Draw();
+  pad8->cd();
+
+  pad8->SetTickx(1);
+  pad8->SetTicky(1);
+  pad8->SetGridx(1);
+  pad8->SetGridy(1);
+  TH1F  *hr7;
+  hr7 = canvas7->DrawFrame(xMin,yMin,xMax,yMax);
+  hr7->SetTitle("#Sigma as fn of Cos#theta_{CM} (E_{#gamma} 540-560MeV)");
+
+  gr15 = new TGraphErrors(10, x, pSigmaValues510, ex, pSigmaErrValues510);
+  gr15->SetMarkerColor(2);
+  gr15->SetMarkerStyle(21);
+  gr15->SetMarkerSize(2);
+  gr16 = new TGraphErrors(10, x, nSigmaValues510, ex, nSigmaErrValues510);
+  gr16->SetMarkerColor(4);
+  gr16->SetMarkerStyle(22);
+  gr16->SetMarkerSize(2);
+  TMultiGraph *mg8 = new TMultiGraph();
+  mg8->Add(gr15, "ep");
+  mg8->Add(gr16, "ep");
+  mg8->SetTitle("#Sigma as fn of Cos#theta_{CM} (E_{#gamma} 540-560MeV)");
+  mg8->GetXaxis()->SetTitle("Cos#theta_{CM}");
+  mg8->GetYaxis()->SetTitle("#Sigma");
+  leg8 = new TLegend(0.75, 0.75, 0.95, 0.95); // Add legend to plot
+  leg8->AddEntry(gr15, "Proton", "ep");
+  leg8->AddEntry(gr16, "Neutron", "ep");
+  mg8->Draw();
+  leg8->Draw("Same");
+
+  TCanvas *canvas8 = new TCanvas("canvas8","canvas8", 1920, 1080);
+  TPad *pad9 = new TPad("pad9","",0,0,1,1);
+  pad9->Draw();
+  pad9->cd();
+
+  pad9->SetTickx(1);
+  pad9->SetTicky(1);
+  pad9->SetGridx(1);
+  pad9->SetGridy(1);
+  TH1F  *hr8;
+  hr8 = canvas7->DrawFrame(xMin,yMin,xMax,yMax);
+  hr8->SetTitle("#Sigma as fn of Cos#theta_{CM} (E_{#gamma} 560-580MeV)");
+
+  gr17 = new TGraphErrors(10, x, pSigmaValues510, ex, pSigmaErrValues510);
+  gr17->SetMarkerColor(2);
+  gr17->SetMarkerStyle(21);
+  gr17->SetMarkerSize(2);
+  gr18 = new TGraphErrors(10, x, nSigmaValues510, ex, nSigmaErrValues510);
+  gr18->SetMarkerColor(4);
+  gr18->SetMarkerStyle(22);
+  gr18->SetMarkerSize(2);
+  TMultiGraph *mg9 = new TMultiGraph();
+  mg9->Add(gr17, "ep");
+  mg9->Add(gr18, "ep");
+  mg9->SetTitle("#Sigma as fn of Cos#theta_{CM} (E_{#gamma} 560-580MeV)");
+  mg9->GetXaxis()->SetTitle("Cos#theta_{CM}");
+  mg9->GetYaxis()->SetTitle("#Sigma");
+  leg9 = new TLegend(0.75, 0.75, 0.95, 0.95); // Add legend to plot
+  leg9->AddEntry(gr17, "Proton", "ep");
+  leg9->AddEntry(gr18, "Neutron", "ep");
+  mg9->Draw();
+  leg9->Draw("Same");
+
+  TCanvas *canvas9 = new TCanvas("canvas9","canvas9", 1920, 1080);
+  TPad *pad10 = new TPad("pad10","",0,0,1,1);
+  pad10->Draw();
+  pad10->cd();
+
+  pad10->SetTickx(1);
+  pad10->SetTicky(1);
+  pad10->SetGridx(1);
+  pad10->SetGridy(1);
+  TH1F  *hr9;
+  hr9 = canvas7->DrawFrame(xMin,yMin,xMax,yMax);
+  hr9->SetTitle("#Sigma as fn of Cos#theta_{CM} (E_{#gamma} 580-600MeV)");
+
+  gr19 = new TGraphErrors(10, x, pSigmaValues510, ex, pSigmaErrValues510);
+  gr19->SetMarkerColor(2);
+  gr19->SetMarkerStyle(21);
+  gr19->SetMarkerSize(2);
+  gr20 = new TGraphErrors(10, x, nSigmaValues510, ex, nSigmaErrValues510);
+  gr20->SetMarkerColor(4);
+  gr20->SetMarkerStyle(22);
+  gr20->SetMarkerSize(2);
+  TMultiGraph *mg10 = new TMultiGraph();
+  mg10->Add(gr19, "ep");
+  mg10->Add(gr20, "ep");
+  mg10->SetTitle("#Sigma as fn of Cos#theta_{CM} (E_{#gamma} 580-600MeV)");
+  mg10->GetXaxis()->SetTitle("Cos#theta_{CM}");
+  mg10->GetYaxis()->SetTitle("#Sigma");
+  leg10 = new TLegend(0.75, 0.75, 0.95, 0.95); // Add legend to plot
+  leg10->AddEntry(gr19, "Proton", "ep");
+  leg10->AddEntry(gr20, "Neutron", "ep");
+  mg10->Draw();
+  leg10->Draw("Same");
+
+  TCanvas *canvas10 = new TCanvas("canvas10","canvas10", 1920, 1080);
+  TPad *pad11 = new TPad("pad11","",0,0,1,1);
+  pad11->Draw();
+  pad11->cd();
+
+  pad11->SetTickx(1);
+  pad11->SetTicky(1);
+  pad11->SetGridx(1);
+  pad11->SetGridy(1);
+  TH1F  *hr10;
+  hr10 = canvas7->DrawFrame(xMin,yMin,xMax,yMax);
+  hr10->SetTitle("#Sigma as fn of Cos#theta_{CM} (E_{#gamma} 600-620MeV)");
+
+  gr21 = new TGraphErrors(10, x, pSigmaValues510, ex, pSigmaErrValues510);
+  gr21->SetMarkerColor(2);
+  gr21->SetMarkerStyle(21);
+  gr21->SetMarkerSize(2);
+  gr22 = new TGraphErrors(10, x, nSigmaValues510, ex, nSigmaErrValues510);
+  gr22->SetMarkerColor(4);
+  gr22->SetMarkerStyle(22);
+  gr22->SetMarkerSize(2);
+  TMultiGraph *mg11 = new TMultiGraph();
+  mg11->Add(gr21, "ep");
+  mg11->Add(gr22, "ep");
+  mg11->SetTitle("#Sigma as fn of Cos#theta_{CM} (E_{#gamma} 600-620MeV)");
+  mg11->GetXaxis()->SetTitle("Cos#theta_{CM}");
+  mg11->GetYaxis()->SetTitle("#Sigma");
+  leg11 = new TLegend(0.75, 0.75, 0.95, 0.95); // Add legend to plot
+  leg11->AddEntry(gr21, "Proton", "ep");
+  leg11->AddEntry(gr22, "Neutron", "ep");
+  mg11->Draw();
+  leg11->Draw("Same");
+
+  TCanvas *canvas11 = new TCanvas("canvas11","canvas11", 1920, 1080);
+  TPad *pad12 = new TPad("pad12","",0,0,1,1);
+  pad12->Draw();
+  pad12->cd();
+
+  pad12->SetTickx(1);
+  pad12->SetTicky(1);
+  pad12->SetGridx(1);
+  pad12->SetGridy(1);
+  TH1F  *hr11;
+  hr11 = canvas7->DrawFrame(xMin,yMin,xMax,yMax);
+  hr11->SetTitle("#Sigma as fn of Cos#theta_{CM} (E_{#gamma} 620-640MeV)");
+
+  gr23 = new TGraphErrors(10, x, pSigmaValues510, ex, pSigmaErrValues510);
+  gr23->SetMarkerColor(2);
+  gr23->SetMarkerStyle(21);
+  gr23->SetMarkerSize(2);
+  gr24 = new TGraphErrors(10, x, nSigmaValues510, ex, nSigmaErrValues510);
+  gr24->SetMarkerColor(4);
+  gr24->SetMarkerStyle(22);
+  gr24->SetMarkerSize(2);
+  TMultiGraph *mg12 = new TMultiGraph();
+  mg12->Add(gr23, "ep");
+  mg12->Add(gr24, "ep");
+  mg12->SetTitle("#Sigma as fn of Cos#theta_{CM} (E_{#gamma} 620-640MeV)");
+  mg12->GetXaxis()->SetTitle("Cos#theta_{CM}");
+  mg12->GetYaxis()->SetTitle("#Sigma");
+  leg12 = new TLegend(0.75, 0.75, 0.95, 0.95); // Add legend to plot
+  leg12->AddEntry(gr21, "Proton", "ep");
+  leg12->AddEntry(gr22, "Neutron", "ep");
+  mg12->Draw();
+  leg12->Draw("Same");
+
+  //TCanvas *canvas12 = new TCanvas("canvas12","canvas12", 1920, 1080);
+  //canvas12->Divide(4,3);
+  //canvas12->cd(1);
+  //pad1->Draw();
+  //canvas12->cd(2);
+  //pad2->Draw();
+  //canvas12->cd(3);
+  //pad3->Draw();
+  //canvas12->cd(4);
+  //pad4->Draw();
+  //canvas12->cd(5);
+  //pad5->Draw();
+  //canvas12->cd(6);
+  //pad6->Draw();
+  //canvas12->cd(7);
+  //pad7->Draw();
+  //canvas12->cd(8);
+  //pad8->Draw();
+  //canvas12->cd(9);
+  //pad9->Draw();
+  //canvas12->cd(10);
+  //pad10->Draw();
+  //canvas12->cd(11);
+  //pad11->Draw();
+  //canvas12->cd(12);
+  //pad12->Draw();
 
   canvas->Write();
   canvas1->Write();
@@ -417,6 +627,12 @@ void Sigma(){
   canvas4->Write();
   canvas5->Write();
   canvas6->Write();
+  canvas7->Write();
+  canvas8->Write();
+  canvas9->Write();
+  canvas10->Write();
+  canvas11->Write();
+  //canvas12->Write();
   f3.Write();
 
 }
